fdfef8733e2d73bcd066b9665d31c20a46cb9f30
Felipe Ruiz <felieruiz.web@gmail.com>
2020-03-02 02:34:08 -0300

    comentários

--- worker/worker.py
+++ worker/worker.py
@@ -8,7 +8,7 @@ from random import randint
 if __name__ == '__main__':
   redis_host = os.getenv('REDIS_HOST', 'queue')
   r = redis.Redis(host=redis_host, port=6379, db=0)
-  print('Aguardando mensagens ...')
+  print('Aguardando novas mensagens ...')
 
   while True:
     mensagem = json.loads(r.blpop('sender')[1])
